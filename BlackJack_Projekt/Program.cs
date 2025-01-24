53152617798f8e6d4652e8037c412da606229213
Marci <Marci@DESKTOP-J8H9JJS>
2025-01-24 12:20:32 +0100

    Kártya értékekek eldöntéséhez tartozo metodus

--- BlackJack_Projekt/Program.cs
+++ BlackJack_Projekt/Program.cs
@@ -3,3 +3,15 @@
 
 string[] jelek = { "Kör", "Káró", "Treff", "Pikk" };
 string[] ertekek = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Bubi", "Dáma", "Király", "Ász" };
+
+
+
+
+
+int KartyaErtek(string lap)
+{
+    if (lap == "Ász") return 11;
+    if (lap == "Király" || lap == "Bubi" || lap == "Dáma") return 10;
+    if (int.TryParse(lap, out int szam)) return szam;
+    return 0;
+}
