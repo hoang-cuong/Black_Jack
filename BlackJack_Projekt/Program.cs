a8813ec5448f23007b233933cf12e4b1e7d3537e
Marci <Marci@DESKTOP-J8H9JJS>
2025-01-24 12:19:25 +0100

    random számgenerátor létrehozása és a kártyák jeleinek és értékeiknek a tombjei létrehozása szintén

--- BlackJack_Projekt/Program.cs
+++ BlackJack_Projekt/Program.cs
@@ -1,1 +1,5 @@
-﻿
+﻿Random random = new Random();
+
+
+string[] jelek = { "Kör", "Káró", "Treff", "Pikk" };
+string[] ertekek = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Bubi", "Dáma", "Király", "Ász" };
